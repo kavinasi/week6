9d3ad7f40bb46317b2532d052e0a088c051e3f1d
Karthikeyan Nadesan <Karthik@Bharathis-Air.attlocal.net>
2022-03-20 00:32:23 -0400

    add

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Main Spring Application yes.
+ * Main Spring Application no.
  */
 @SpringBootApplication
 @EnableCaching
